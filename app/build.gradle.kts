42e5e89f706d166d203229a389639dd9e632c864
om tiwary <om1.tiwary@ril.com>
2025-05-16 16:39:59 +0530

    Initial Project Setup 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.op.friends"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.op.friends"
@@ -30,8 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
